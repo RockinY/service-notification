e969924905422a61053464bee312d40e0f06ba70
Bran Liang <lby89757@hotmail.com>
2018-08-02 21:29:23 +0800

    Complete listeners

--- src/listeners/notifications.js
+++ src/listeners/notifications.js
@@ -3,3 +3,21 @@ import {
   listenToNewDirectMessageNotifications
 } from '../models/notification'
 import { sendNotificationAsPushQueue } from '../utils/queues'
+
+const sendDeduplicatedPushNotification = notification => {
+  // By using notification.id and notification.userId here we make sure that even when multiple instances of athena are running
+  // and are adding this job to the queue it gets deduplicated
+  sendNotificationAsPushQueue.add(
+    { notification },
+    {
+      jobId: `notification-${notification.id}-${notification.userId}-${
+        notification.modifiedAt
+      }`,
+    }
+  );
+};
+
+export default () => {
+  listenToNewNotifications(sendDeduplicatedPushNotification);
+  listenToNewDirectMessageNotifications(sendDeduplicatedPushNotification);
+};
